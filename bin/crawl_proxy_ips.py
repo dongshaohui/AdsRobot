acec0072fc2abe93ebb321822da22f2ed1a7fd6d
dongshaohui <dongsh@jsfund.cn>
2015-10-11 22:49:48 +0800

    爬取proxy ip port

--- bin/crawl_proxy_ips.py
+++ bin/crawl_proxy_ips.py
@@ -5,6 +5,7 @@
 import DB
 from BeautifulSoup import BeautifulSoup
 import sys,os,urllib2,threading
+import socket
 
 # 连接数据库 
 def Connent_Online_Mysql_By_DB(hostname,port,username,pwd,dbname,socket):
@@ -22,12 +23,25 @@ def write_record_db(db,list_obj,table_name):
 # 获取代理信息
 def fetch_proxy_info(db):
 	# clean proxy list
-	db.delete_table()
-	fetch_anony_proxy_info()
+	db.delete_table('proxy_ip_list') # 删除proxy ip list
+	fetch_anony_proxy_info(db)
 
 # 获取普通代理信息
-def fetch_normal_proxy_info():
+def fetch_normal_proxy_info(db):
 	root = 'http://www.xicidaili.com/nt/'
+
+# 检测端口是否开放
+def check_port_status(ip,port):
+	sc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	sc.settimeout(5)
+	try:
+		sc.connect((ip,(int)(port)))
+		sc.close()
+		del sc
+		return True
+	except:
+		return False
+	
 
 # 获取匿名代理信息
 def fetch_anony_proxy_info(db):
@@ -37,10 +51,8 @@ def fetch_anony_proxy_info(db):
 		curl_cmd = 'curl ' + page_link + ' > ../data/' + (str)(i) + '.data'
 		os.system(curl_cmd)
 		f = open('../data/' + (str)(i) + '.data' , 'r')
-		
 		# 解析内容，获取proxy ip
 		soup = BeautifulSoup(f.read())
-		
 		trlist = soup.find('table',{'id':'ip_list'}).findAll('tr')[1:]
 		for trinfo in trlist:
 			ip_address = trinfo.findAll('td')[2].text
@@ -50,14 +62,16 @@ def fetch_anony_proxy_info(db):
 			record['ip_address'] = ip_address
 			record['ip_port'] = ip_port
 			record['ip_type'] = ip_type
-			write_record_db(db,record,'proxy_ip_list')
-			f.close()
+			if check_port_status(ip_address,ip_port) == True:
+				write_record_db(db,record,'proxy_ip_list')
+			else:
+				print ip_address,ip_port,' - invalid'
+		f.close()
 		del_cmd = 'rm -rf ' + '../data/' + (str)(i) + '.data' 
 		os.system(del_cmd)
 
 				
 if __name__ == '__main__':
 	# db connector
-	
-	db = Connent_Online_Mysql_By_DB('rdsjjuvbqjjuvbqout.mysql.rds.aliyuncs.com',3306,'dongsh','5561225','financal_product','/tmp/mysql.sock')
+	db = Connent_Online_Mysql_By_DB('rdsjjuvbqjjuvbqout.mysql.rds.aliyuncs.com',3306,'dongsh','5561225','ad_robot_db','/tmp/mysql.sock')
 	fetch_proxy_info(db)
